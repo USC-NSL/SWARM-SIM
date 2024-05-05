9ba6bb5e804b8a7c3dd34ebb09d6bb682a97eb9d
aghavidel <arvinghavidel@gmail.com>
2024-05-05 16:28:59 -0700

    haddop action scripts

---
+++ utils/sample-all-actions-hadoop-metal.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./sample-all-actions.sh <n>"
+    exit 1;
+fi
+
+template_no_action="mpiexec -np 40 %s \
+    --mpi --podLps=32 --coreLps=8 --offloadAgg --offloadCore \ 
+    --numPods=8 --switchRadix=8 --numServers=4 --linkRate=20Gbps --linkDelay=100us \
+    --end=10.0 \
+    --monitor --noAcks --until=1.0 \
+    --flow=traffic-hadoop$1.txt --out=fct-no-action-$1 --scenario=scenario-no-action.txt"
+
+template_disable_high="mpiexec -np 40 %s \
+    --mpi --podLps=32 --coreLps=8 --offloadAgg --offloadCore \ 
+    --numPods=8 --switchRadix=8 --numServers=4 --linkRate=20Gbps --linkDelay=100us \
+    --end=10.0 \
+    --monitor --noAcks --until=1.0 \
+    --flow=traffic-hadoop$1.txt --out=fct-disable-high-$1 --scenario=scenario-disable-high-loss.txt"
+
+template_disable_low="mpiexec -np 40 %s \
+    --mpi --podLps=32 --coreLps=8 --offloadAgg --offloadCore \ 
+    --numPods=8 --switchRadix=8 --numServers=4 --linkRate=20Gbps --linkDelay=100us \
+    --end=10.0 \
+    --monitor --noAcks --until=1.0 \
+    --flow=traffic-hadoop$1.txt --out=fct-disable-low-$1 --scenario=scenario-disable-low-loss.txt"
+
+template_disable_both="mpiexec -np 40 %s \
+    --mpi --podLps=32 --coreLps=8 --offloadAgg --offloadCore \ 
+    --numPods=8 --switchRadix=8 --numServers=4 --linkRate=20Gbps --linkDelay=100us \
+    --end=10.0 \
+    --monitor --noAcks --until=1.0 \
+    --flow=traffic-hadoop$1.txt --out=fct-disable-both-$1 --scenario=scenario-disable-both.txt"
+
+echo "******** NO ACTION ********"
+../ns3/ns3 run --command-template="$template_no_action" swarm
+
+echo "******** HIGH LOSS DOWN ********"
+../ns3/ns3 run --command-template="$template_disable_high" swarm
+
+echo "******** LOW LOSS DOWN ********"
+../ns3/ns3 run --command-template="$template_disable_low" swarm
+
+echo "******** BOTH LINKS DOWN ********"
+../ns3/ns3 run --command-template="$template_disable_both" swarm
