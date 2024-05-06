2e276bc47f03d61b6546b9752f39103fa03e457c
aghavidel <arvinghavidel@gmail.com>
2024-05-05 19:48:37 -0700

    Reduced hadoop traffic time to 2 seconds

--- utils/setup-hadoop-experiment.sh
+++ utils/setup-hadoop-experiment.sh
@@ -8,10 +8,10 @@ fi
 echo "Copying scenarios"
 cp ./scenarios/*.txt ../ns3
 
-echo "Creating $1 traffic files (FbHdp.txt, 128 hosts, 0.7 load for 5 seconds)"
+echo "Creating $1 traffic files (FbHdp.txt, 128 hosts, 0.7 load for 2 seconds)"
 cd ./../hpcc/traffic_gen/
 for ((i=1; i<=$1; i++))
 do
-   ./traffic_gen.py -c FbHdp.txt -n 128 -l 0.7 -t 5 -b 20G -o ../../ns3/"traffic-hadoop$i.txt"
+   ./traffic_gen.py -c FbHdp.txt -n 128 -l 0.7 -t 2 -b 20G -o ../../ns3/"traffic-hadoop$i.txt"
 done
 
